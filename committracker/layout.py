e8cdc205216e5994d9db8be39860952670e2daa1
andrecosta99 <afmcosta@gmail.com>
2024-03-18 12:53:14 +0000

    Update the UI for better usability

--- committracker/layout.py
+++ committracker/layout.py
@@ -2,48 +2,52 @@ from dash import html, dcc
 import dash_bootstrap_components as dbc
 from .plugin_loader import load_plugins
 
-
-PLUGIN_TITLES = {
-    'git_statistics': 'Git Statistics',
-    'branch_information': 'Branch Information',
-    'commit_graph': 'Commit Graph',
-
-}
-
 def create_layout(app):
     plugins = load_plugins()
     plugin_options = [
-        {'label': PLUGIN_TITLES.get(plugin, ' '.join(plugin.replace('_', ' ').title())), 'value': plugin}
+        {'label': plugin.replace('_', ' ').title(), 'value': plugin}
         for plugin in plugins.keys()
     ]
 
-    navbar = dbc.NavbarSimple(
-        brand="Commit Tracker",
-        brand_href="#",
+    navbar = dbc.Navbar(
+        dbc.Container(
+            [
+                dbc.Row(
+                    [
+                        dbc.Col(html.Img(src="https://assets.streamlinehq.com/image/private/w_200,h_200,ar_1/f_auto/v1/icons/freebies-freemojis/objects/objects/bar-chart-f5c5npy7d6s2nmc8ttmgxd.png?_a=DAJFJtWIZAAC", height="30px", className="me-3")),
+                        dbc.Col(dbc.NavbarBrand("Commit Tracker", className="ms-2")),
+                    ],
+                    align="center",
+                    className="g-0",
+                ),
+            ],
+            fluid=True,
+        ),
         color="primary",
         dark=True,
-        className="mb-4"
+        className="mb-3",
     )
 
     plugin_selector = dcc.Dropdown(
         id='plugin-selector',
         options=plugin_options,
         multi=True,
-        placeholder='Select plugins...'
+        placeholder='Select plugins...',
+        className='mb-2'
     )
 
     repo_input = dbc.Input(
         id='repo-input',
         type='text',
         placeholder='Enter repository URL or path...',
-        className='me-2'
+        className='mb-2'
     )
 
     submit_button = dbc.Button(
         'Load Repository',
         id='load-repo-button',
-        n_clicks=0,
-        color="primary"
+        color="primary",
+        className='mb-4'
     )
 
     plugin_output_area = html.Div(id='plugin-output-area')
@@ -52,18 +56,24 @@ def create_layout(app):
         navbar,
         dbc.Container(
             [
-                dbc.Row([
-                    dbc.Col(plugin_selector, md=8),
-                    dbc.Col(submit_button, md=4)
-                ], className="mb-3"),
-                dbc.Row([
-                    dbc.Col(repo_input, md=12)
-                ], className="mb-3"),
-                dbc.Row([
-                    dbc.Col(plugin_output_area, md=12)
-                ])
+                dbc.Row(
+                    dbc.Col(plugin_selector, width=12, lg=8),
+                    justify="center",
+                ),
+                dbc.Row(
+                    dbc.Col(repo_input, width=12, lg=8),
+                    justify="center",
+                ),
+                dbc.Row(
+                    dbc.Col(submit_button, width=12, lg=8),
+                    justify="center",
+                ),
+                dbc.Row(
+                    dbc.Col(plugin_output_area, md=8),
+                    justify="center",
+                ),
             ],
-            fluid=True
+            fluid=True,
         )
     ])
 
